fbdec1582547ad598c16845111e79843e918f13d
Ngo Thanh Tung <tung.ngo@outlook.com>
2016-07-02 17:35:50 +0700

    Add project files.

---
+++ ApiExample/Controllers/ProductsController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+using ApiExample.Models;
+
+namespace ApiExample.Controllers
+{
+    public class ProductsController : ApiController
+    {
+        private OnlineShopEntities db = new OnlineShopEntities();
+        public IEnumerable<Product> GetAllProducts()
+        {
+            db.Configuration.LazyLoadingEnabled = false;
+            db.Configuration.ProxyCreationEnabled = false;
+            return db.Products.ToList();
+        }
+
+        public IHttpActionResult GetProduct(int id)
+        {
+            db.Configuration.LazyLoadingEnabled = false;
+            db.Configuration.ProxyCreationEnabled = false;
+
+            var product = db.Products.Find(id);
+            if (product == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(product);
+        }
+    }
+}
